8eded4d037ea99af056f145253993ebbb2f6b940
Harish <harishvenkata65@gmail.com>
2025-06-11 14:43:06 +0530

    created home and navbar components and images are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Home from "./components/Home";
 
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      {" "}
+      <Home />{" "}
+    </div>
+  );
 }
