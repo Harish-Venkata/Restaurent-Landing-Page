f41cd167de66eb9b44005cd3cd7dc67854fe77c5
Harish <harishvenkata65@gmail.com>
2025-06-12 12:55:26 +0530

    nav component responsive done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,15 @@ import React, { useState } from "react";
 import Logo from "../assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
@@ -39,7 +48,7 @@ const Navbar = () => {
       <div>
         <img src={Logo} alt="" className="md:max-w-lg h-auto mr-80 mt-0" />
       </div>
-      <div className=" flex mt-5">
+      <div className=" flex mt-5 hidden md:flex">
         <a
           href="#"
           className="mr-12 no-underline text-black text-[1.1rem] font-semibold"
@@ -71,16 +80,33 @@ const Navbar = () => {
           {" "}
           <BsCart2 />{" "}
         </a>
-        <button className="bg-white outline-0 border-none rounded-3xl p-4 pt-2 pr-4 cursor-pointer duration-75 mt-0 text-black font-bold">
+        <button className="bg-white outline-0 border-none rounded-3xl p-4 pt-2 pr-4 cursor-pointer duration-75 mt-0 text-black font-bold md:w-48 md:-mr-10">
           Bookings Now
         </button>
       </div>
-      <div className="hidden">
+      <div className="lg:hidden md:hidden sm:block">
         <HiOutlineBars3
           onClick={() => setOpenMenu(true)}
-          className="rounded-2xl cursor-pointer"
+          className="rounded-2xl cursor-pointer sm:ml-60 text-black font-bold sm:-mt-3"
         />
       </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </nav>
   );
 };
